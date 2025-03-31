aca4c46254150c4a8c9684727dad4ca527dbd8c2
Daniel Vargas <work@danielv.co>
2025-03-31 18:50:22 +0000

    Adapted the folder name blog to projects. and it's imports

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,8 +1,8 @@
 import { BlogPosts } from 'app/components/posts'
 
 export const metadata = {
-  title: 'Blog',
-  description: 'Read my blog.',
+  title: 'Projects',
+  description: 'Checkout my Projects',
 }
 
 export default function Page() {
